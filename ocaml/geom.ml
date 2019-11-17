9ebd7c582a8a949f851964db563d899b704c8236
Ricardo M. Correia <rcorreia@wizy.org>
2019-11-17 03:42:01 +0100

    Add comments and simple optimization.

--- ocaml/geom.ml
+++ ocaml/geom.ml
@@ -1,3 +1,10 @@
+(*
+ * Find the maximum rectangle filled with the same color.
+ *
+ * Uses the most naive possible algorithm (brute force) for
+ * simplicity (ah!). It's more than fast enough for grids
+ * this size.
+ *)
 let find_max_rectangle input =
 	let max_area = ref 0 in
 	let duplicate = ref false in
@@ -7,6 +14,7 @@ let find_max_rectangle input =
 	let max_y = Array.length input - 1 in
 	let max_x = Array.length input.(0) - 1 in
 
+	(* Check that the rectangle is filled with the same color *)
 	let check_filled ll_x ll_y ur_x ur_y =
 		let color = input.(ll_y).(ll_x) in
 		let rows = Array.sub input ll_y (ur_y - ll_y + 1) in
@@ -15,14 +23,20 @@ let find_max_rectangle input =
 			Array.for_all (Int.equal color) cells in
 		Array.for_all check_row rows in
 
+	(* Compute the area of the rectangle *)
 	let get_area ll_x ll_y ur_x ur_y =
 		(ur_x - ll_x + 1) * (ur_y - ll_y + 1) in
 
+	(*
+	 * If this rectangle has sufficient area, check that it's filled
+	 * with the same color. If so, either mark it as a duplicate
+	 * rectangle (same area as previous record) or set a new record.
+	 *)
 	let process_rectangle ll_x ll_y ur_x ur_y =
-		let filled = check_filled ll_x ll_y ur_x ur_y in
-		if filled then begin
-			let area = get_area ll_x ll_y ur_x ur_y in
-			if area >= !max_area then begin
+		let area = get_area ll_x ll_y ur_x ur_y in
+		if area >= !max_area then begin
+			let filled = check_filled ll_x ll_y ur_x ur_y in
+			if filled then begin
 				if area = !max_area then
 					duplicate := true
 				else begin
@@ -34,8 +48,10 @@ let find_max_rectangle input =
 			end
 		end in
 
-	(* Let's use the naive algorithm, as it's fast enough *)
-	(* Extreme indentation follows for fun and profit *)
+	(*
+	 * Let's use the naive algorithm, as it's fast enough.
+	 * Extreme indentation follows for fun and profit.
+	 *)
 	for ll_y = 0 to max_y do
 		for ll_x = 0 to max_x do
 			for ur_y = ll_y to max_y do
@@ -50,4 +66,3 @@ let find_max_rectangle input =
 		None
 	else
 		Some (!best_ll, !best_ur)
-
